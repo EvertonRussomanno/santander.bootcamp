c3271be8cb8d83ab3d653a7586bc4941c96d430f
Everton Russomanno <evertonmartins154@gmail.com>
2023-09-24 14:56:34 -0300

    Implementação da camada de domínio

---
+++ src/main/java/me/dio/domain/model/Account.java
@@ -0,0 +1,32 @@
+package me.dio.domain.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_account")
+@Getter
+@Setter
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    private String agency;
+
+    @Column(scale = 13, precision = 2)
+    private BigDecimal balance;
+
+    @Column(name = "additional_limit", scale = 13, precision = 2)
+    private BigDecimal limit;
+
+
+}
